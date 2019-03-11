03d57e77e8a398086719ff989a6dce4da5f7ca14
krasilovamaria <marivasi11@yandex.ru>
2019-03-11 15:22:02 +0700

    Правки перед второй защитой

--- js/gllacy.js
+++ js/gllacy.js
@@ -1,12 +1,64 @@
- var button = document.querySelector(".callback-link");
- var popup = document.querySelector(".modal-feedback");
- var close = popup.querySelector(".modal-close");
+var mapBtn = document.querySelector('.callback-link');
+var feedback = document.querySelector('.modal-feedback');
+var overlay = document.querySelector('.overlay');
+var closeFeedback = document.querySelector('.modal-close');
+var inputName = document.querySelector('.callback-name');
+var inputEmail = document.querySelector('.callback-email');
+var feedbackSubmit = document.querySelector('.feedback-button');
 
- button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("popup-clik");
+var isStorageSupport = true;
+var storage = '';
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+};
+
+feedbackSubmit.addEventListener('click', function(evt) {
+  if (!inputName.value || !inputEmail.value) {
+    evt.preventDefault();
+    feedback.classList.remove('feedback-shake');
+    feedback.offsetWidth = feedback.offsetWidth;
+    feedback.classList.add('feedback-shake');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('login', inputName.value);
+    }
+  }
 });
- close.addEventListener("click", function (evt) {
+
+mapBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
-  popup.classList.remove("popup-clik");
+  feedback.classList.add('feedback-showed');
+  overlay.style.display = 'block';
+  if (storage) {
+    inputName.value = storage;
+    inputEmail.focus();
+  } else {
+    inputName.focus();
+  }
+});
+
+closeFeedback.addEventListener('click', function() {
+  feedback.classList.remove('feedback-showed');
+  overlay.style.display = 'none';
+  feedback.classList.remove('feedback-shake');
+});
+
+overlay.addEventListener('click', function() {
+  this.style.display = 'none'
+  feedback.classList.remove('feedback-showed');
+  feedback.classList.remove('feedback-shake');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    feedback.classList.remove('feedback-shake');
+    if (feedback.classList.contains('feedback-showed')) {
+      feedback.classList.remove('feedback-showed');
+      overlay.style.display = 'none';
+    }
+  }
 });
